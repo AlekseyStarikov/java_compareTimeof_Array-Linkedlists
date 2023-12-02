06ef2e2de7cdec755e0deba015d2557406e3127f
AlekseyStarikov <148627614+AlekseyStarikov@users.noreply.github.com>
2023-12-02 23:48:28 +0300

    Main.java

--- src/main/java/expressionsolve/lab/Main.java
+++ src/main/java/expressionsolve/lab/Main.java
@@ -1,19 +1,25 @@
 package expressionsolve.lab;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int count =20000;
+        TimeCounter time=new TimeCounter();
+        System.out.println("method add()");
+        System.out.println("LinkedList\t"+time.addToLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+time.addToArrayList(count)+"ms");
+        System.out.println("method get()");
+        System.out.println("LinkedList\t"+time.getFromLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+time.getFromArrayList(count)+"ms");
+        System.out.println("method equals()");
+        System.out.println("LinkedList\t"+time.equalsLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+time.equalsArrayList(count)+"ms");
+        System.out.println("method clone()");
+        System.out.println("LinkedList\t"+time.cloneLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+time.cloneArrayList(count)+"ms");
+        System.out.println("method remove()");
+        System.out.println("LinkedList\t"+time.removeFromLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+time.removeFromArrayList(count)+"ms");
     }
 }
