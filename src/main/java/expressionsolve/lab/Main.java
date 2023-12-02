6a7c5e0e3ea029ac6396862b3f2beafea95fc288
AlekseyStarikov <148627614+AlekseyStarikov@users.noreply.github.com>
2023-12-03 00:02:56 +0300

    Main.java

--- src/main/java/expressionsolve/lab/Main.java
+++ src/main/java/expressionsolve/lab/Main.java
@@ -7,19 +7,19 @@ public class Main {
         int count =20000;
         TimeCounter time=new TimeCounter();
         System.out.println("method add()");
-        System.out.println("LinkedList\t"+time.addToLinkedList(count)+"ms");
-        System.out.println("ArrayList\t"+time.addToArrayList(count)+"ms");
+        System.out.println("LinkedList\t"+"вызван\t"+count+"раз\t"+time.addToLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+"вызван\t"+count+"раз\t"+time.addToArrayList(count)+"ms");
         System.out.println("method get()");
-        System.out.println("LinkedList\t"+time.getFromLinkedList(count)+"ms");
-        System.out.println("ArrayList\t"+time.getFromArrayList(count)+"ms");
+        System.out.println("LinkedList\t"+"вызван\t"+count+"раз\t"+time.getFromLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+"вызван\t"+count+"раз\t"+time.getFromArrayList(count)+"ms");
         System.out.println("method equals()");
-        System.out.println("LinkedList\t"+time.equalsLinkedList(count)+"ms");
-        System.out.println("ArrayList\t"+time.equalsArrayList(count)+"ms");
+        System.out.println("LinkedList\t"+"вызван\t"+count+"раз\t"+time.equalsLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+"вызван\t"+count+"раз\t"+time.equalsArrayList(count)+"ms");
         System.out.println("method clone()");
-        System.out.println("LinkedList\t"+time.cloneLinkedList(count)+"ms");
-        System.out.println("ArrayList\t"+time.cloneArrayList(count)+"ms");
+        System.out.println("LinkedList\t"+"вызван\t"+count+"раз\t"+time.cloneLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+"вызван\t"+count+"раз\t"+time.cloneArrayList(count)+"ms");
         System.out.println("method remove()");
-        System.out.println("LinkedList\t"+time.removeFromLinkedList(count)+"ms");
-        System.out.println("ArrayList\t"+time.removeFromArrayList(count)+"ms");
+        System.out.println("LinkedList\t"+"вызван\t"+count+"раз\t"+time.removeFromLinkedList(count)+"ms");
+        System.out.println("ArrayList\t"+"вызван\t"+count+"раз\t"+time.removeFromArrayList(count)+"ms");
     }
 }
